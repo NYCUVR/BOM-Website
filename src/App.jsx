01e401328176fc349e2592938ceb930b34ebc768
蔡語宸 <genden80@gmail.com>
2025-07-02 14:25:51 +0800

    Add: Pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import MainLayout from './components/MainLayout';
 import Homepage from './pages/Homepage';
 import AboutPage from './pages/AboutPage';
 import CarPage from './pages/CarPage';
+import ServicesPage from './pages/ServicesPage';
+import PartnersPage from './pages/PartnersPage';
+import NewsPage from './pages/NewsPage';
+import ContactPage from './pages/ContactPage';
 // We will import other pages here as we create them
 
 function App() {
@@ -13,6 +17,10 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/car" element={<CarPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/partners" element={<PartnersPage />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
           {/* Other routes will be added here */}
         </Routes>
       </MainLayout>
