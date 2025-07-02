b2f9f0bac35cf13dfb2c63afd67dc0aaa875cb95
蔡語宸 <genden80@gmail.com>
2025-07-02 16:10:28 +0800

    Fix: CI/CD

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// base
 export default defineConfig({
   base: '/',
   plugins: [react()],
